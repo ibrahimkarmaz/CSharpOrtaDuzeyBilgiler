c39a778fe0c796e5ccbee7313dc57e5cdc3b540d
İbrahim KARMAZ <107946057+ibrahimkarmaz@users.noreply.github.com>
2022-07-20 23:39:22 +0300

    NKatmanli Mimari Dersleri
    Yapılanlar,
    Katmanlar Oluşturuldu
    DataAccesLayer,BusinessLayer,EntitiyLayer,Ve Projelayeri(Otomatik olarak)
    EntitiyLayer Kısmında Class(Tablo) oluşturuldu berekli bağlantılar verildi.
    Entitiy Framewrk 6 yüklendi tüm katmanlara
    DataAcessLayer kısmına Context Eklendi ve DbContext Kısmına bağlantı DbContext için entitiy framework sınıfı kullanıldı.
    Tablolar bu sınıfta görselinmesi için referanslara gelip EntitiyLayer katmanı eklendi.
    
    Bağlantı adresi ve database verileri eklemek için;
    Proje(Oluşturulan sunum katmanı içinde) AppConfig kısmına ekleme yapıldı
    Buradan sonra Tabloları database eklemek için;(Burada dikkat edilmesi gereken nokta;DataAccessLayer ben tablolarımı nerede görderdim ?)
    Package Manager Console yardımıyla ile;
    1-enable-migrations komutu ile tablolarda,baglanti adresinde vs sorun var mı ?
    yoksa;Migrations klasörü oluşturur.
    
    Klasör oluşturduktan sonra;
    update database ile veritabani işlemleri gerçekleşmiş olur

---
+++ NKatmanliMimarideProjeGelistirme/DataAccessLayer/Context.cs
@@ -0,0 +1,16 @@
+﻿using EntitiyLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class Context:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categorys { get; set; }
+    }
+}
---
+++ NKatmanliMimarideProjeGelistirme/EntitiyLayer/Entities/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntitiyLayer.Entities
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+        [StringLength(50)]
+        public string CategoryName { get; set; }
+
+        public ICollection<Product> Product { get; set; }
+    }
+}
