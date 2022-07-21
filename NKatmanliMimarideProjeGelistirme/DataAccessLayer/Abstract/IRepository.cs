73d6d66a4358e547fc69bcccbb39444c1cb50433
İbrahim KARMAZ <107946057+ibrahimkarmaz@users.noreply.github.com>
2022-07-21 23:52:23 +0300

    Repository İşlemleri
    Bu işlemlerde Belirli işlemlerin tekrarlanmaması için interface yardımıyla Repostory oluşturuldu.
    Peki Repostory Nedir?
    Veritabani işlemlerinin tem bir noktadan yönetilmesi özetlemek gerekirse budur.
    
    interface interface olarak dal üretibiliyoruz
    örnek vermek gerekirse ;
    Bizim repostory interface'in içini bir tablo için işlemleri alıyoruz bu interface ile temel işlemleri yapabiliyoruz eğer daha fazlasi gerekiyorsa buradaki kalıtım aldığımız interface te değişiklik yapıyoruz.

---
+++ NKatmanliMimarideProjeGelistirme/DataAccessLayer/Abstract/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Abstract
+{//T:Entitiy
+    //Repository amacı veritabani işlemlerini tek bir noktadan yönetmek.
+    public interface IRepository<T>
+    {
+        int insert(T p);
+        int Update(T p);
+        int Delete(T p);
+        List<T> List();
+        T GetById(int id);
+
+    }
+}
